77ecaad7809947c819ca21540823839c20d580a5
daffaputraa <daffaputrapratamaa@gmail.com>
2025-08-21 17:45:45 +0700

    DF-FIX : Link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 const NotFound = () => {
@@ -7,12 +8,12 @@ const NotFound = () => {
       <div className="flex h-screen items-center justify-center flex-col">
         <h1 className="text-4xl font-bold">Under Maintanance</h1>
         <p className="mt-2 text-lg">Halaman yang kamu cari dalam maintanance.</p>
-        <a
+        <Link
           href="/"
           className="mt-4 px-4 py-2 bg-[#2F346C] text-white rounded-md hover:bg-blue-700"
         >
           Kembali ke Beranda
-        </a>
+        </Link>
       </div>
     </div>
   );
